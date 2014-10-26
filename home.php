3559f52b962570dbd957697ecb1bf1445b9f9cab
Onno Bos <bosonno@gmail.com>
2014-10-26 13:58:33 +0100

    Site title and description on homepage

--- home.php
+++ home.php
@@ -14,11 +14,8 @@ get_header(); ?>
         <!-- Banner -->
             <section id="banner">
                 <header>
-                    <h2>Hi. You're looking at <strong>Helios</strong>.</h2>
-                    <p>
-                        A (free) responsive site template by <a href="//html5up.net">HTML5 UP</a>.
-                        Built on <strong>skel</strong> and released under the <a href="http://html5up.net/license">CCA</a> license.
-                    </p>
+                    <h2><?php bloginfo( 'name' ); ?></h2>
+                    <p><?php bloginfo( 'description' ); ?></p>
                 </header>
             </section>
 
