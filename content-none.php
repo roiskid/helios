1a01dc4f23a76294ff6a6c893a7f47edbf4e9a22
obos <bosonno@gmail.com>
2014-11-04 20:36:32 +0100

    content none template

--- content-none.php
+++ content-none.php
@@ -25,12 +25,10 @@
                 <?php elseif ( is_search() ) : ?>
 
                 <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'wphelios' ); ?></p>
-                <?php get_search_form(); ?>
 
                 <?php else : ?>
 
                 <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'wphelios' ); ?></p>
-                <?php get_search_form(); ?>
 
                 <?php endif; ?>
 
