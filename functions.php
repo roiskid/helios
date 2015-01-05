c44b7b492b617127195fea9c13cc2874baaa51b0
obos <bosonno@gmail.com>
2015-01-05 18:58:35 +0100

    change in snowflake date

--- functions.php
+++ functions.php
@@ -170,7 +170,7 @@ add_action( 'admin_init', 'wphelios_page_cats_and_tags' );
 function wphelios_enqueue_scripts() {
     wp_enqueue_script('wp-helios', get_template_directory_uri() . '/js/wp-helios.min.js', array('jquery'), null, true );
     wp_enqueue_script('init', get_template_directory_uri() . '/js/init.min.js', array('wp-helios'), null, true );
-    if (date('md') >= '1224' || date('md') < '0201') {
+    if (date('md') >= '1224' || date('md') <= '0101') {
         wp_enqueue_script('snow', get_template_directory_uri() . '/js/snow.js', array('wp-helios'), null, true );
     }
 }
